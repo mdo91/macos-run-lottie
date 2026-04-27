74b32946595602b6ee410ad1011148d0d2a5a63a
Mahmut Aoata <hesoka_mxx@yahoo.com>
2026-04-27 14:07:45 +0300

    Initial Commit

---
+++ Run Lottie/Run_LottieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_LottieApp.swift
+//  Run Lottie
+//
+//  Created by Mahmoud Aoata on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Run_LottieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
